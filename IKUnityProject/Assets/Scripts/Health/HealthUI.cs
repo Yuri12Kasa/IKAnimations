7323386872ab92a2297418fdaac6d6f294f50766
Yuri12Kasa <yuri12kasa@gmail.com>
2025-09-08 23:40:54 +0200

    added health bar UI animation

--- IKUnityProject/Assets/Scripts/Health/HealthUI.cs
+++ IKUnityProject/Assets/Scripts/Health/HealthUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,20 +9,49 @@ namespace Health
     {
         [ReadOnly] [SerializeField] private HealthComponent _healthComponent;
         [SerializeField] private Image _healthBar;
+        [SerializeField] private float _resizingDuration = 0.2f;
 
         private int _maxHealth;
+        private int _currentHealth;
+        private Coroutine _resizingCoroutine;
 
         public void SetHealthComponent(HealthComponent healthComponent)
         {
             _healthComponent = healthComponent;
-            _healthComponent.OnChangeHealth += ResizeHealthBar;
+            _healthComponent.OnChangeHealth += StartResizeAnimation;
             _maxHealth = _healthComponent.MaxHealth;
             ResizeHealthBar(_healthComponent.CurrentHealth);
         }
 
-        private void ResizeHealthBar(int currentHealth)
+        private void ResizeHealthBar(int newCurrentHealth)
         {
-            _healthBar.fillAmount = currentHealth / (float)_maxHealth;
+            _healthBar.fillAmount = newCurrentHealth / (float)_maxHealth;
+            _currentHealth = newCurrentHealth;
+        }
+
+        private void StartResizeAnimation(int newCurrentHealth)
+        {
+            if(_resizingCoroutine != null)
+                StopCoroutine(_resizingCoroutine);
+            
+            _resizingCoroutine = StartCoroutine(ResizeHealthBarCoroutine(newCurrentHealth));
+        }
+
+        private IEnumerator ResizeHealthBarCoroutine(int newCurrentHealth)
+        {
+            var timer = 0f;
+            var startValue = _currentHealth;
+            var endValue = (float)newCurrentHealth;
+            while (timer <= _resizingDuration)
+            {
+                timer += Time.deltaTime;
+                var t = timer / _resizingDuration;
+                _healthBar.fillAmount = Mathf.Lerp(startValue, endValue, t) / _maxHealth;
+                _currentHealth = newCurrentHealth;
+                yield return null;
+            }
+
+            _resizingCoroutine = null;
         }
     }
 }
